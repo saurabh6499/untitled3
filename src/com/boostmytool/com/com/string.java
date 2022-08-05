32c2d0491676b58191e210bd51a871a505dae41c
niet <saurav6499@>
2022-08-05 09:54:30 -0700

    string

--- src/com/boostmytool/com/com/string.java
+++ src/com/boostmytool/com/com/string.java
@@ -1,14 +1,2 @@
-package com.boostmytool.com.com;
 
-public class string {
-    public static void main(String[] args)
-    {
-        StringBuffer r = new StringBuffer("saurabh yadav");
-
-
-
-            System.out.println(r.reverse());
-
-        }
-    }
 
