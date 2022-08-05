1b33f66e0c283b3b2b3d7c25fd530ad418336221
niet <saurav6499@>
2022-08-05 09:49:49 -0700

    string

---
+++ src/com/boostmytool/com/com/Numbertonumbername.java
@@ -0,0 +1,56 @@
+package com.boostmytool;
+
+import java.util.Scanner;
+
+
+public class Numbertonumbername {
+
+
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+    int number=0;
+
+    number=sc.nextInt();
+        {
+            System.out.println('enter the number');
+        }
+    if(number==0) {
+        System.out.println('zero');
+    }
+    else if(number==1) {
+        System.out.println('one');
+    }
+    else if(number==2) {
+        System.out.println('two');
+    }
+    else if(number==3) {
+        System.out.println('three')
+    }
+    else if(number==4) {
+        System.out.println('four');
+    }
+    else if (number=5) {
+        System.out.println('five');
+    }
+    else if (number=6) {
+        System.out.println('six');
+    }
+    else if (number==7) {
+        System.out.println('seven');
+    }
+    else if (number==8) {
+        System.out.println('eight');
+    }
+    else if(number==9) {
+        System.out.println('nine');
+    }
+    else
+        {
+            System.out.println('enter wrong no.')
+        }
+
+
+    }
+
+}
