df27c985abd4aa42884fdf5c1d2eb9e1a0581b17
niet <saurav6499@>
2022-08-16 20:01:28 -0700

    java

--- src/com/boostmytool/MainClass.java
+++ src/com/boostmytool/MainClass.java
@@ -3,14 +3,14 @@ package com.boostmytool;
 public class MainClass { public static void main(String[]args)
 
 {
-    int a=20;
-    int b=50;
+    int a=236;
+    int b=5;
     int sum;
 
 
 
 
-    sum=a+b;
+    sum=a%b;
 
 
     System.out.println(+sum);
--- src/com/boostmytool/com/com/Numbertonumbername.java
+++ src/com/boostmytool/com/com/Numbertonumbername.java
@@ -1,56 +1,61 @@
-package com.boostmytool;
+package com.boostmytool.com.com;
 
 import java.util.Scanner;
 
 
-public class number{
+class Number {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        int rem = 0;
 
-    public static void main(String[]args)
-    {
-        Scanner sc=new Scanner(System.in);
-    int number=0;
+        System.out.print("enter the number");
+        number = sc.nextInt();
+        int n = number;
+        int reverse = 0;
 
-    number=sc.nextInt();
-        {
-            System.out.println('enter the number');
+        while (n > 0) {
+            rem = n % 10;
+            reverse = reverse * 10 + rem;
+            n = n / 10;
         }
-    if(number==0) {
-        System.out.println("zero");
-    }
-    else if(number==1) {
-        System.out.println("one");
-    }
-    else if(number==2) {
-        System.out.println("two" );
-    }
-    else if(number==3) {
-        System.out.println("three")
-    }
-    else if(number==4) {
-        System.out.println("four");
-    }
-    else if (number=5) {
-        System.out.println("five");
-    }
-    else if (number=6) {
-        System.out.println("six");
-    }
-    else if (number==7) {
-        System.out.println("seven");
-    }
-    else if (number==8) {
-        System.out.println("eight");
-    }
-    else if(number==9) {
-        System.out.println("nine");
-    }
-    else
-        {
-            System.out.println("enter wrong no.")
+        while (reverse > 0) {
+            rem = reverse % 10;
+
+            reverse = reverse / 10;
+
+
+            //  System.out.print(rem);
+
+
+            if (rem == 0) {
+                {
+                    System.out.print("zero");
+                }
+            } else if (rem == 1) {
+                System.out.print("one");
+            } else if (rem == 2) {
+                System.out.print("two");
+            } else if (rem == 3) {
+                System.out.print("three");
+            } else if (rem == 4) {
+                System.out.print("four");
+            } else if (rem == 5) {
+                System.out.print("five");
+            } else if (rem == 6) {
+                System.out.print("six");
+            } else if (rem == 7) {
+                System.out.print("seven");
+            } else if (rem == 8) {
+                System.out.print("eight");
+            } else if (rem == 9) {
+                System.out.print("nine");
+            } else {
+                System.out.print("enter wrong no.");
+            }
         }
 
-
     }
-
 }
+
